feb551657787be92de8c82cbd57f833894cf8e1b
Arjav Choudhary <arjavchoudhary608@gmail.com>
2024-08-21 15:06:13 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button, link} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link} from "@nextui-org/react";
 import Logo from '@/public/horizaura.svg'
 import Image from 'next/image'
 import { ModeToggle } from "./themeswitcher";
@@ -13,6 +13,7 @@ export default function Header() {
     { name: "Blog", link: "/blog" },
     { name: "Contact Us", link: "/Contact_Us" },
   ];
+
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen} className="py-5">
       <NavbarContent>
@@ -21,44 +22,45 @@ export default function Header() {
           className="sm:hidden"
         />
         <NavbarBrand>
-        <Image src={Logo} className="max-md:h-1/2 max-md:w-1/3" height={80} width={80} alt="nirvatatva Logo" priority />
-          <p className=" p-5 sm:p-0 max-md:p-0 max-md:text-3xl text-4xl font-semibold text-inherit">nirvatatva</p>
+          <Image 
+            src={Logo} 
+            className="max-md:h-1/2 max-md:w-1/3" 
+            height={80} 
+            width={80} 
+            alt="nirvatatva Logo" 
+            priority 
+          />
+          <p className="p-5 sm:p-0 max-md:p-0 max-md:text-3xl text-4xl font-semibold text-inherit">nirvatatva</p>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4 text-4xl" justify="center">
-        <NavbarItem>
-          <Link color="foreground" className="font-semibold text-xl" href="/">
-            Home
-          </Link>
-        </NavbarItem>
-        <NavbarItem >
-        <Link color="foreground" className="font-semibold text-xl" href="/store">
-            Store
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-        <Link color="foreground" className="font-semibold text-xl" href="/blog">
-            Blog
-          </Link>
-        </NavbarItem>
-        <NavbarItem >
-        <Link color="foreground" className="font-semibold text-xl" href="/Contact_Us">
-            Contact Us
-          </Link>
-        </NavbarItem>
+        {menuItems.map(item => (
+          <NavbarItem key={item.name}>
+            <Link color="foreground" className="font-semibold text-xl" href={item.link}>
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
+
       <NavbarContent justify="end">
         <NavbarItem>
           <ModeToggle />
         </NavbarItem>
       </NavbarContent>
+
+      {/* Mobile Menu */}
       <NavbarMenu>
-        {menuItems.map((item) => (
-          <NavbarMenuItem key={item.name}>
-            <Link href={item.link}>{item.name}</Link>
-          </NavbarMenuItem>
-        ))}
+        <ul className="flex flex-col gap-4">
+          {menuItems.map((item) => (
+            <li key={item.name}>
+              <NavbarMenuItem>
+                <Link href={item.link}>{item.name}</Link>
+              </NavbarMenuItem>
+            </li>
+          ))}
+        </ul>
       </NavbarMenu>
     </Navbar>
   );
